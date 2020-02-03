ef34d4382c8ff0fe309def84f69f7f43bb667a07
Ivan Prokofyev <ivan.prokofyev@gmail.com>
2020-02-03 17:23:39 +0300

    fix IP

--- for_windows/start_vlm_as_client.bat
+++ for_windows/start_vlm_as_client.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\VideoLAN\VLC\vlc.exe" rtsp://192.168.18.21:8554/virtcam --no-qt-error-dialogs --loop --no-audio --no-video-title-show
+"C:\Program Files\VideoLAN\VLC\vlc.exe" rtsp://192.168.18.20:8554/virtcam --no-qt-error-dialogs --loop --no-audio --no-video-title-show
